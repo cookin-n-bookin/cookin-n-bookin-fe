5b5289f33575aa22d85ce63091b0d8fe3d260241
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-06-01 15:19:56 -0700

    styled menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import AuthButton from '../AuthButton/AuthButton'
+import AuthButton from '../AuthButton/AuthButton';
 import { useUser } from '../../hooks/user';
 import { useAuth } from '../../hooks/user';
 import { useState } from 'react';
@@ -20,37 +20,47 @@ export default function Header() {
     setCloseNav(true);
     setOpenNav(false);
   }
-  
 
-  
-  
   return (
     <>
       <div>
-        <button className={style.openMain}
+        <button
+          className={style.openMain}
           onClick={handleOpen}
-        style={openNav ? {marginLeft: '250px'} : {marginLeft: '0px'}}>☰ Menu</button>
+          style={openNav ? { marginLeft: '250px' } : { marginLeft: '0px' }}
+        >
+          ☰ Menu
+        </button>
       </div>
-      <div className={style.sidebar}
-          style={closeNav ? {width: '0px'} : {width: '250px'}}>
-        <p className={style.closeBtn} onClick={handleClose}>X</p>
-        {!isLoggedIn ? 
-        <><NavLink activeClassName='active' to='/books'>
-            Browse Books
-          </NavLink><AuthButton /></>  
-         : <><NavLink activeClassName='active' to='/books'>
-            Browse Books
-          </NavLink>
-            <NavLink activeClassName='active' to='/books/new'>
+      <div
+        className={style.sidebar}
+        style={closeNav ? { width: '0px' } : { width: '250px' }}
+      >
+        <p className={style.closeBtn} onClick={handleClose}>
+          X
+        </p>
+        {!isLoggedIn ? (
+          <>
+            <NavLink activeClassName={style.active} to="/books">
+              Browse Books
+            </NavLink>
+            <AuthButton />
+          </>
+        ) : (
+          <>
+            <NavLink activeClassName={style.active} to="/books">
+              Browse Books
+            </NavLink>
+            <NavLink activeClassName={style.active} to="/books/new">
               Add New Book
             </NavLink>
-            <NavLink activeClassName='active' to={`/users/${user.id}/books`}>
+            <NavLink activeClassName={style.active} to={`/users/${user.id}/books`}>
               My Shelf
             </NavLink>
             <AuthButton />
           </>
-        }
+        )}
       </div>
     </>
-  )
+  );
 }
