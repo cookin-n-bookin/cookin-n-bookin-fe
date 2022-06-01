b9c16f5f9416a2969b6933183660300b43dc2d7d
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-06-01 10:01:37 -0700

    added books fetch call in utils

---
+++ src/utils/BooksFetch.js
@@ -0,0 +1,10 @@
+
+export default function BooksFetch() {
+const res = await fetch(
+    'https://cookin-n-bookin-be.herokuapp.com/api/v1/books'
+);
+
+const results = await res.json();
+
+return results;
+}
