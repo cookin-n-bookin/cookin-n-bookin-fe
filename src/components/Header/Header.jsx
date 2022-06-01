2f6859d3a160d2dbba22642ca84bab84138884fc
Denzel <dbartolaba123@gmail.com>
2022-06-01 11:39:24 -0700

    creating a nav slider and have an handle open/close function

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,31 @@
 import AuthButton from '../AuthButton/AuthButton'
 import { useUser } from '../../hooks/user';
+import { useState } from 'react';
+import style from './Header.css';
 
 export default function Header() {
-  const { currentUser} = useUser();
+  const { currentUser } = useUser();
+  const [openNav, setOpenNav] = useState(false);
+  const [closeNav, setCloseNav] = useState(true);
+
+  function handleOpen() {
+    setOpenNav(true);
+    setCloseNav(false);
+  }
+
+  function handleClose() {
+    setCloseNav(true);
+    setOpenNav(false);
+  }
+  
+
   
   
   return (
     <>
-      <div>Header</div>
+      <div>
+        <button className={style.openBtn} onClick={handleOpen}>☰ Menu</button>
+      </div>
       <AuthButton />
     </>
   )
