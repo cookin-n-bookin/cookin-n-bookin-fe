9507bfe5bfdf4792ac605b85948403a2cf41c07f
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-06-01 15:43:32 -0700

    staged commits

--- src/components/Books/BookItem.jsx
+++ src/components/Books/BookItem.jsx
@@ -1,13 +1,18 @@
 import { Link } from 'react-router-dom';
+import { Image } from 'cloudinary-react';
 
 export default function BookItem({ book }) {
   return (
     <>
     <div>
         <Link to={`/books/${book.id}`}>
+        <Image 
+            style={{ width: 200 }}
+            cloudName="dwwab45mm" 
+            publicId={book.imageId}
+          />
             <h4>{book.title}</h4>
             <p>{book.author}</p>
-            <p>{book.imageId}</p>
         </Link>
     </div>
     </>
