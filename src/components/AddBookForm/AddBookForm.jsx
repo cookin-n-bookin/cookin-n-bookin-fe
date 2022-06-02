67e4b033b53c4c47aaad25fd2e05bc232bfb0b09
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-06-02 10:40:38 -0700

    added styles to add book page

--- src/components/AddBookForm/AddBookForm.jsx
+++ src/components/AddBookForm/AddBookForm.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react';
 import useForm from '../../hooks/useForm';
 import CloudinaryInput from '../Cloudinary/CloudinaryInput';
-// import { Image } from 'cloudinary-react'
 import useCloudinary from '../../hooks/cloudinary';
-
+import styles from './AddBook.css';
 
 export default function AddBookForm({ label, onSubmit }) {
   const { formState, handleChange } = useForm({
     title: '',
     author: '',
-    imageId: ''
+    imageId: '',
   });
 
   const [error, setError] = useState('');
@@ -19,58 +18,59 @@ export default function AddBookForm({ label, onSubmit }) {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const { title, author } = formState;
-    // console.log('imageId', imageId)
-    console.log('publicId', publicId)
+    console.log('publicId', publicId);
     try {
       await onSubmit(title, author, publicId);
     } catch (error) {
       setError(error);
     }
   };
-  
+
   return (
     <>
-      <section>
-        <p>Book Cover</p>
-        <CloudinaryInput 
-        publicId={publicId}
-        setUploadedImage={setUploadedImage}
-        uploadImage={uploadImage}
-        />
-      </section>
-
-      <form aria-label="form" onSubmit={handleSubmit}>
+      <div className={styles.addContainer}>
         <section>
-          <input
-            type="text"
-            name="title"
-            placeholder="Book Title"
-            required
-            value={formState.title}
-            onChange={handleChange}
+          <CloudinaryInput
+            publicId={publicId}
+            setUploadedImage={setUploadedImage}
+            uploadImage={uploadImage}
           />
         </section>
 
-        <section>
-          <input
-            type="text"
-            name="author"
-            placeholder="Book Author"
-            required
-            value={formState.author}
-            onChange={handleChange}
-          />
-        </section>
+        <form
+          aria-label="form"
+          onSubmit={handleSubmit}
+          className={styles.addForm}
+        >
+          <section>
+            <input
+              type="text"
+              name="title"
+              placeholder="Book Title"
+              required
+              value={formState.title}
+              onChange={handleChange}
+            />
+          </section>
 
-        <button 
-        type="submit" 
-        onSubmit={handleSubmit}
-        disabled={!publicId}>
-          Submit
-        </button>
-      </form>
-      
-      <p>{error.message}</p>
+          <section>
+            <input
+              type="text"
+              name="author"
+              placeholder="Book Author"
+              required
+              value={formState.author}
+              onChange={handleChange}
+            />
+          </section>
+
+          <button type="submit" onSubmit={handleSubmit} disabled={!publicId}>
+            Submit
+          </button>
+        </form>
+
+        <p>{error.message}</p>
+      </div>
     </>
   );
 }
