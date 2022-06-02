3cf29115a8e3e375021c1d0e32a01f5764f1e236
Trevor Rezac <trev.rezac@gmail.com>
2022-06-02 14:29:08 -0700

    current book owners display, need to add link to their shelf

--- src/components/DetailTabs/OwnersTab.jsx
+++ src/components/DetailTabs/OwnersTab.jsx
@@ -1,9 +1,26 @@
 import styles from './StylesTab.css';
+import { useBooks } from '../../hooks/books';
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import { useEffect } from 'react';
 
 export default function OwnersTab() {
+  const { bookOwners, getBookOwners } = useBooks();
+  const { id } = useParams();
+
+  useEffect(() => {
+    const setBookOwnerList = async () => {
+      await getBookOwners(id);
+    }
+    setBookOwnerList();
+  }, [])
+
+
+  console.log('bookOwners in OwnersTab', bookOwners)
+
   return (
     <div>
-      <p> current book owners go here</p>
+      {bookOwners.map((owner) => <p>{owner.username}</p>)}
+
     </div>
   )
 }
