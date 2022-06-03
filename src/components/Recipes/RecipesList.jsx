90f2c2b3d0b7ed90843a81d788114c4d88c7d12b
Trevor Rezac <trev.rezac@gmail.com>
2022-06-02 20:29:15 -0700

    renderd recipes on recipe tab

--- src/components/Recipes/RecipesList.jsx
+++ src/components/Recipes/RecipesList.jsx
@@ -1,26 +1,29 @@
 import { useEffect } from 'react';
 import { useRecipes } from '../../hooks/recipes';
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
 
 export default function RecipesList() {
     const { recipes, getAllRecipes } = useRecipes();
+    const { id } = useParams();
 
+    useEffect(() => {
+    if (recipes.length) return null;
 
-  useEffect(() => {
-    if (!recipes.length) return null;
     const fetchRecipes = async () => {
       await getAllRecipes();
     };
     fetchRecipes();
   }, []);
 
-
+  const filteredRecipes = recipes.filter((recipe) => recipe.bookId === id) 
+  
   return (
     <>
       <div>RecipesList</div>
       <div>
-        {recipes.map((recipe) => {
+        {filteredRecipes.map((recipe) => {
           return (
-            <div>
+            <div key={`${recipe.id}-${recipe.title}`}>
               <p>{recipe.title}</p>
               <p>{recipe.pageNumber}</p>
             </div>
--- src/hooks/recipes.js
+++ src/hooks/recipes.js
@@ -6,34 +6,34 @@ import {
 } from '../services/recipes';
 
 export function useRecipes() {
-    const [recipes, setRecipes] = useState([]);
-    const [updatedRecipe, setUpdatedRecipe] = useState({});
-    const [newRecipe, setNewRecipe] = useState({});
-    const [isAddingRecipe, setIsAddingRecipe] = useState(false);
+  const [recipes, setRecipes] = useState([]);
+  const [updatedRecipe, setUpdatedRecipe] = useState({});
+  const [newRecipe, setNewRecipe] = useState({});
+  const [isAddingRecipe, setIsAddingRecipe] = useState(false);
 
-    const getAllRecipes = async () => {
-        const fetchedRecipes = await fetchAllRecipes();
-        setRecipes(fetchedRecipes);
-    };
+  const getAllRecipes = async () => {
+    const fetchedRecipes = await fetchAllRecipes();
+    setRecipes(fetchedRecipes);
+  };
 
-    const editRecipe = async (id) => {
-        const edited = await updateRecipeById(id);
-        setUpdatedRecipe(edited);
-    };
+  const editRecipe = async (id) => {
+    const edited = await updateRecipeById(id);
+    setUpdatedRecipe(edited);
+  };
 
-    const addRecipe = async () => {
-        const added = await insertRecipe();
-        setNewRecipe(added);
-    }
+  const addRecipe = async () => {
+    const added = await insertRecipe();
+    setNewRecipe(added);
+  };
 
   return {
-      recipes, 
-      updatedRecipe, 
-      newRecipe,
-      getAllRecipes,
-      editRecipe,
-      addRecipe,
-      isAddingRecipe,
-      setIsAddingRecipe
+    recipes,
+    updatedRecipe,
+    newRecipe,
+    getAllRecipes,
+    editRecipe,
+    addRecipe,
+    isAddingRecipe,
+    setIsAddingRecipe,
   };
 }
