11798a7f968805dc1f9a02ff62f7e1865d111eb4
Victor Caruso <vic.caruso@gmail.com>
2022-06-02 22:14:12 -0700

    undid changes to button

--- src/components/Books/BookItem.jsx
+++ src/components/Books/BookItem.jsx
@@ -1,37 +1,20 @@
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { Image } from 'cloudinary-react';
 import styles from './Books.css';
 import { useUser } from '../../hooks/user';
 import { addBookToUser } from '../../services/books';
-import { useEffect } from 'react';
 
-export default function BookItem({ book, userBooks }) {
+export default function BookItem({ book }) {
   const { user, isLoading } = useUser();
+  const { history } = useHistory();
 
   const handleClick = async () => {
     await addBookToUser(user.id, book.id);
+    history.replace(`/users/${user.id}/books`);
   };
 
   if (isLoading) return null;
-  if (!userBooks)
-    return (
-      <>
-        <div className={styles.book}>
-          <Link to={`/books/${book.id || book.book_id}`}>
-            <Image
-              className={styles.image}
-              cloudName="dwwab45mm"
-              publicId={book.imageId || book.image_id}
-            />
-            <h1 className={styles.bookTitle}>{book.title}</h1>
-            <p className={styles.author}>{book.author}</p>
-          </Link>
-        </div>
-      </>
-    );
 
-  const onShelf = !!userBooks.find((myBook) => book.id === myBook.book_id);
-  console.log(onShelf);
   return (
     <>
       <div className={styles.book}>
@@ -44,13 +27,9 @@ export default function BookItem({ book, userBooks }) {
           <h1 className={styles.bookTitle}>{book.title}</h1>
           <p className={styles.author}>{book.author}</p>
         </Link>
-        {!onShelf ? (
-          <button className={styles.addButton} onClick={handleClick}>
-            Add to My Shelf
-          </button>
-        ) : (
-          <p>On your shelf</p>
-        )}
+        <button className={styles.addButton} onClick={handleClick}>
+          Add to My Shelf
+        </button>
       </div>
     </>
   );
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -8,26 +8,21 @@ import styles from '../../components/Books/Books.css';
 export default function Home() {
   const { user, isLoading } = useUser();
   const { bookList, getAllBooks } = useBooks();
-  const { userBookList, getUserBooks } = useBooks();
 
   useEffect(() => {
     if (isLoading && !user.id) return null;
     const fetchBookList = async () => {
       await getAllBooks();
     };
-    const fetchUserBookList = async (id) => {
-      await getUserBooks(id);
-    };
     fetchBookList();
-    fetchUserBookList(user.id);
-  }, [user.id]);
+  }, []);
 
   return (
     <div>
       <Header />
 
       <h1 className={styles.mainShelf}>All The Cooks Books</h1>
-      <BooksList bookList={bookList} userBooks={userBookList} />
+      <BooksList bookList={bookList} />
     </div>
   );
 }
